d285b2daffaf2acad7aa0324017e198360c01951
Cam0411 <89090731+Cam0411@users.noreply.github.com>
2021-09-04 15:57:56 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
     $(window).scroll(function(){
         var scroll = $(window).scrollTop();
-        if (scroll > 100) {
+        if (scroll > 100 && scroll < 600) {
           $("#header").css("background" , "#0004", );
+        }else if (scroll > 700){
+          $("#header").css("background" , "#37517e", );
         }
   
         else{
